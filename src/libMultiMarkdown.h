3db334282314416ced81ad9458dd83977f368cef
Fletcher T. Penney <fletcher@fletcherpenney.net>
2015-11-12 13:46:56 -0500

    merge pull request from MMD-4

--- src/libMultiMarkdown.h
+++ src/libMultiMarkdown.h
@@ -110,18 +110,18 @@ enum parser_extensions {
 /* Define output formats we support -- first in list is default */
 enum export_formats {
 	ORIGINAL_FORMAT,                /* Transclusion happens, but no parsing */
-	HTML_FORMAT,
-	TEXT_FORMAT,                    /* Not currently used */
+	HTML_FORMAT,                    /* Well supported */
+	TEXT_FORMAT,                    /* Not currently used, may exit host process */
 	LATEX_FORMAT,
 	MEMOIR_FORMAT,
 	BEAMER_FORMAT,
 	OPML_FORMAT,
 	ODF_FORMAT,
-	RTF_FORMAT,
+	RTF_FORMAT,                     /* Not recommended for production code, may crash */
 	CRITIC_ACCEPT_FORMAT,
 	CRITIC_REJECT_FORMAT,
 	CRITIC_HTML_HIGHLIGHT_FORMAT,
-	LYX_FORMAT,
+	LYX_FORMAT,                     /* Not actively developed */
 	TOC_FORMAT,
 };
 
